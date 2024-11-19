4cd6b7f458e8ab92e1b6068ab9f71e4a5dea4d13
Linh4767 <ngoclinh24032004@gmail.com>
2024-11-19 21:28:04 +0700

    QuangVinh_ASP

--- ProjectASP_Nhom8/Models/DoanhNghiep.cs
+++ ProjectASP_Nhom8/Models/DoanhNghiep.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectASP_Nhom8.Models
 {
@@ -9,10 +10,13 @@ namespace ProjectASP_Nhom8.Models
         {
             ChuyenThamQuans = new HashSet<ChuyenThamQuan>();
         }
+        [Display(Name = "Mã")]
+        public string MaDn { get; set; }
+        [Display(Name = "Tên Doanh Nghiệp")]
+        public string TenDn { get; set; }
 
-        public string MaDn { get; set; } = null!;
-        public string TenDn { get; set; } = null!;
-        public string DiaChi { get; set; } = null!;
+        [Display(Name = "Địa Chỉ")]
+        public string DiaChi { get; set; }
 
         public virtual ICollection<ChuyenThamQuan> ChuyenThamQuans { get; set; }
     }
